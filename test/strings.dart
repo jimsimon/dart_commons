4ed8175884a046cfb670f38763531fdd412769f5
Jim Simon <jim.j.simon@gmail.com>
2013-04-08 23:13:42 -0400

    Initial commit of string utility class

---
+++ test/strings.dart
@@ -0,0 +1,18 @@
+import "package:dart_commons/language.dart";
+import "package:unittest/unittest.dart";
+
+main() {
+  group("isNullOrEmpty()", () {
+    test("returns true for null", () {
+      expect(Strings.isNullOrEmpty(null), true);
+    });
+    
+    test("returns true for empty string", () {
+      expect(Strings.isNullOrEmpty(""), true);
+    });
+    
+    test("returns false for non-empty string", () {
+      expect(Strings.isNullOrEmpty("not empty"), false);
+    });
+  });
+}
