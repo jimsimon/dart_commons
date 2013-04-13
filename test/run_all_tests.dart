067c51617e9156099050da237e6365c8e89586fb
Jim Simon <jim.j.simon@gmail.com>
2013-04-12 22:42:43 -0400

    Build script cleanup

--- test/run_all_tests.dart
+++ test/run_all_tests.dart
@@ -1,8 +1,6 @@
 library run_all_tests;
 
-import 'dart:io';
 import 'package:unittest/compact_vm_config.dart';
-import 'package:unittest/unittest.dart';
 import "dart:mirrors";
 
 import "src/language/autocloseable_test.dart" as autocloseable_test;
@@ -11,8 +9,6 @@ import "preconditions_test.dart" as preconditions_test;
 import "src/language/strings_test.dart" as strings_test;
 
 main() {
-  var args = new Options().arguments;
-  var pattern = new RegExp(args.length > 0 ? args[0] : '.');
   useCompactVMConfiguration();
   
   //Use reflection to run all tests in any library ending with "_test"
