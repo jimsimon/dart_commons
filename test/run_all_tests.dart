596ef0bfbce896f0707e5d0e916258042c5efc1c
Jim Simon <jim.j.simon@gmail.com>
2013-05-17 01:02:57 -0400

    Fixed test runner failing due to changes in dart:mirrors

--- test/run_all_tests.dart
+++ test/run_all_tests.dart
@@ -3,6 +3,7 @@ library run_all_tests;
 import 'package:unittest/compact_vm_config.dart';
 import "package:unittest/unittest.dart";
 import "dart:mirrors";
+import "dart:uri";
 
 import "src/io/autocloseable_test.dart" as autocloseable_test;
 import "src/language/objects_test.dart" as objects_test;
@@ -14,8 +15,8 @@ main() {
   
   //Use reflection to run all tests in any library ending with "_test"
   MirrorSystem ms = currentMirrorSystem();
-  ms.libraries.forEach((Symbol libName, LibraryMirror lm) {
-    if (MirrorSystem.getName(libName).endsWith("_test")) {
+  ms.libraries.forEach((Uri libName, LibraryMirror lm) {
+    if (libName.toString().endsWith("_test.dart")) {
       group("$libName:", () {
         lm.invokeAsync(const Symbol("main"), []);
       });
