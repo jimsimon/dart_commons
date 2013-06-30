87f468b30dfe7da46f3bb0eec24114d3072414cd
Jim Simon <jim.j.simon@gmail.com>
2013-06-30 13:01:22 -0400

    Added Strings.replaceLast and corresponding tests

--- lib/src/language/strings.dart
+++ lib/src/language/strings.dart
@@ -32,4 +32,39 @@ class Strings {
     }
     return null;
   }
+  
+  /**
+   * Returns a new string where the last occurence of [from] in this string is replaced with [to].
+   * 
+   * If [from] does not match any part of the string, then the original string is returned unmodified.
+   */
+  static String replaceLast(String source, Pattern from, String to) {
+    if (source == null) {
+      throw new ArgumentError("source String cannot be null");
+    }
+    if (from == null) {
+      throw new ArgumentError("from Pattern cannot be null");
+    }
+    if (to == null) {
+      throw new ArgumentError("to String cannot be null");
+    }
+    
+    Iterable<Match> matches = from.allMatches(source);
+    
+    if (matches.isEmpty) {
+      return source;
+    }
+      
+    Match match = matches.last;
+    String matchedString = match.group(match.groupCount);
+    
+    int lastIndex = source.lastIndexOf(matchedString);
+    if (lastIndex == -1) {
+      return source;
+    }
+    
+    String firstPart = source.substring(0, lastIndex);
+    String secondPart = source.substring(lastIndex + matchedString.length, source.length);
+    return "$firstPart$to$secondPart";
+  }
 }
