12a8ea6d37562b161cdbfb8cbdc3cd68a81fbd3d
Jim Simon <jim.j.simon@gmail.com>
2013-07-25 22:03:51 -0400

    Cleaned up some test output verbiage

--- test/src/preconditions_test.dart
+++ test/src/preconditions_test.dart
@@ -4,20 +4,20 @@ part of tests;
 preconditions() {
   
   group("checkThat()", () {
-    test("throws error on invalid condition", () {
+    test("throws an error on an invalid condition", () {
       expect(() => checkThat(3, lessThan(2)), throwsA(new isInstanceOf<PreconditionError>()));
     });
     
-    test("executes normally on valid condition", () {
+    test("executes normally on a valid condition", () {
       expect(() => checkThat(3, greaterThan(2)), returnsNormally);
     });
     
-    test("returns checked value on valid condition", (){
+    test("returns the checked value on a valid condition", (){
       expect(checkThat("a string", new isInstanceOf<String>()), "a string");
     });
     
     //TODO Find a better way to test custom error messages  
-    test("throws error with custom message", () {
+    test("throws an error with a specific custom message", () {
       String customMessage = "Must be over 9000!";
       expect(() => checkThat(9000, greaterThan(9000), customMessage),
           throwsA(predicate((PreconditionError e) => e.message == customMessage)));
